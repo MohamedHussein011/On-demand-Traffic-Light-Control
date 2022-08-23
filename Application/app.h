e248e93b28d0d6dc5dc16524c4f98a3577c4f242
Mohamed Hussein <100514033+MohamedHussein011@users.noreply.github.com>
2022-08-23 06:47:27 +0200

    Create app.h

---
+++ Application/app.h
@@ -0,0 +1,13 @@
+/*
+ * app.h
+ *
+ *  Created on: 19/08/2022
+ *      Author: Mohamed Hussein
+ */
+
+#ifndef _APPLICATION_APP_H
+#define _APPLICATION_APP_H
+
+void app_Start(void);
+
+#endif /* APPLICATION_APP_H_ */
