f463ee82f3bd40059861c66f1f9a251d940b23b3
Mohamed Hussein <100514033+MohamedHussein011@users.noreply.github.com>
2022-08-23 06:54:09 +0200

    Create EXI_private.h

---
+++ MCAL/External_Interrupt/EXI_private.h
@@ -0,0 +1,27 @@
+/*
+ * Name: Mohamed Hussein 
+ * Layer: MCAL
+ * Version: V1.0 - 28/10/2021
+ * Driver: External Interrupts
+ */
+ 
+
+#ifndef _EXI_PRIVATE_H
+#define _EXI_PRIVATE_H
+
+/******************************************************************************
+* !comment : Select Interrupt Sense Control option:  		 	               *
+*           1: LOW_LEVEL                                                     *
+*           2: ON_CHANGE                                                     *
+            3: FALLING_EDGE                                                     *
+            4: RISING_EDGE                                         			*
+******************************************************************************/
+ 
+#define LOW_LEVEL        1
+#define ON_CHANGE        2
+#define FALLING_EDGE     3
+#define RISING_EDGE      4
+
+ 
+ 
+#endif 
